0dbf58661256a6d16ba3b77408e3a46cdee1e15f
universoti <64614511+universoti@users.noreply.github.com>
2022-05-30 14:05:54 -0300

    segundo comit , retirada da chamada padrao de exemplo

--- ApiPopulacaoEstados/Program.cs
+++ ApiPopulacaoEstados/Program.cs
@@ -39,19 +39,19 @@ app.MapGet("/PopulacaoPorUf", (string xsigla) =>
 }
 );
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+//app.MapGet("/weatherforecast", () =>
+//{
+//    var forecast = Enumerable.Range(1, 5).Select(index =>
+//       new WeatherForecast
+//       (
+//           DateTime.Now.AddDays(index),
+//           Random.Shared.Next(-20, 55),
+//           summaries[Random.Shared.Next(summaries.Length)]
+//       ))
+//        .ToArray();
+//    return forecast;
+//})
+//.WithName("GetWeatherForecast");
 
 app.Run();
 
